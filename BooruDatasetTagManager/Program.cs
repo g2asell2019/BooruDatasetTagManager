e034fea0b95b3edb826a3a98d17456a8e324a2e2
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2023-03-03 19:14:22 +0700

    remove wrong library

--- BooruDatasetTagManager/Program.cs
+++ BooruDatasetTagManager/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Translator;
-using Newtonsoft.Json;
 using System.IO;
 using Newtonsoft.Json;
 using System.Threading;
